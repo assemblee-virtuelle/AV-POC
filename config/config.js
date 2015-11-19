d57dae0e2489cc2b871de26c8b8c043e539fecda
balessan <benoit.alessandroni@gmail.com>
2015-11-19 18:50:09 +0100

    Now displaying both actors and projects

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 var config = {
-  'containerUrl': 'http://localhost/balessan/ldp_resource/',
+  'actorContainerUrl': 'http://localhost/balessan/ldp_container/actor/',
+  'projectContainerUrl': 'http://localhost/balessan/ldp_container/project/',
   'contextUrl': 'http://owl.openinitiative.com/oicontext.jsonld'
 }
