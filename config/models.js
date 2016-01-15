9c0c1d28a35990ca400403cee87d425961fffd6b
balessan <benoit.alessandroni@gmail.com>
2016-01-15 11:01:03 +0100

    Add form rendering on the POC

---
+++ config/models.js
@@ -0,0 +1,237 @@
+var models = {
+  "project": {
+    "fields": [
+      {
+        "title": "Project title",
+        "name": "ldp_project_title",
+        "type": "text"
+      },
+      {
+        "title": "Project description",
+        "name": "ldp_project_description",
+        "type": "textarea"
+      },
+      {
+        "title": "Project context",
+        "name": "ldp_project_context",
+        "type": "textarea"
+      },
+      {
+        "title": "Project vision",
+        "name": "ldp_project_vision",
+        "type": "textarea"
+      },
+      {
+        "title": "Project goal",
+        "name": "ldp_project_goal",
+        "type": "textarea"
+      },
+      {
+        "title": "Project missions",
+        "name": "ldp_project_missions",
+        "type": "textarea"
+      },
+      {
+        "title": "Project inspiration sources",
+        "name": "ldp_project_inspiration_sources",
+        "type": "textarea"
+      },
+      {
+        "title": "Project organization model",
+        "name": "ldp_project_organization_model",
+        "type": "textarea"
+      },
+      {
+        "title": "Project economical model",
+        "name": "ldp_project_economical_model",
+        "type": "textarea"
+      },
+      {
+        "title": "Project ecological model",
+        "name": "ldp_project_ecological_model",
+        "type": "textarea"
+      },
+      {
+        "title": "Project technological model",
+        "name": "ldp_project_technological_model",
+        "type": "textarea"
+      },
+      {
+        "title": "Project legal framework",
+        "name": "ldp_project_legal_framework",
+        "type": "textarea"
+      },
+      {
+        "title": "Contributeur 1 (URL)",
+        "name": "ldp_project_contributes"
+      },
+      {
+        "title": "Contributeur 2 (URL)",
+        "name": "ldp_project_contributes2"
+      },
+      {
+        "title": "Contributeur 3 (URL)",
+        "name": "ldp_project_contributes3"
+      },
+      {
+        "title": "Contributeur 4 (URL)",
+        "name": "ldp_project_contributes4"
+      },
+      {
+        "title": "Contributeur 5 (URL)",
+        "name": "ldp_project_contributes5"
+      },
+      {
+        "title": "Projet associé : (URL)",
+        "name": "ldp_related_to"
+      },
+      {
+        "title": "Projet associé : (URL)",
+        "name": "ldp_related_to2"
+      }
+    ]
+  },
+  "resource": {
+    "fields": [
+      {
+        "title": "Resource title",
+        "name": "ldp_title"
+      },
+      {
+        "title": "Resource URL, if any",
+        "name": "ldp_resource_url"
+      },
+      {
+        "title": "Resource type",
+        "name": "ldp_resource_type"
+      }
+    ]
+  },
+  "idea": {
+    "fields": [
+      {
+        "title": "Idea Title",
+        "name": "ldp_title"
+      },
+      {
+        "title": "Idea description",
+        "name": "ldp_description",
+        "type": "textarea"
+      },
+      {
+        "title": "Website/URL (if any)",
+        "name": "ldp_website_url"
+      },
+      {
+        "title": "Wikipedia link (if any)",
+        "name": "ldp_wikipedia_url"
+      },
+      {
+        "title": "Description Excerpt (in a few words)",
+        "name": "ldp_description_excerpt"
+      }
+    ]
+  },
+  "actor": {
+    "fields": [
+      {
+        "title": "What is your name ?",
+        "name": "ldp_name"
+      },
+      {
+        "title": "What is your firstname ?",
+        "name": "ldp_firstname"
+      },
+      {
+        "title": "Who are you (in a few words) ?",
+        "name": "ldp_description_excerpt",
+        "type": "textarea"
+      },
+      {
+        "title": "Who are you (longer version)?",
+        "name": "ldp_description",
+        "type": "textarea"
+      },
+      {
+        "title": "Profile Picture URL",
+        "name": "ldp_profile_picture_url"
+      },
+      {
+        "title": "Projet (URL)",
+        "name": "ldp_contributes_to"
+      },
+      {
+        "title": "Projet 2 (URL)",
+        "name": "ldp_contributes_to2"
+      },
+      {
+        "title": "Projet 3 (URL)",
+        "name": "ldp_contributes_to3"
+      },
+      {
+        "title": "Projet 4 (URL)",
+        "name": "ldp_contributes_to4"
+      },
+      {
+        "title": "What is your pseudo ?",
+        "name": "ldp_pseudo"
+      },
+      {
+        "title": "What is your birthdate ?",
+        "name": "ldp_birthdate"
+      },
+      {
+        "title": "What is your address ?",
+        "name": "ldp_address"
+      },
+      {
+        "title": "What is your zipcode ?",
+        "name": "ldp_zipcode"
+      },
+      {
+        "title": "What city do you live in ?",
+        "name": "ldp_city"
+      },
+      {
+        "title": "Personal Website URL ?",
+        "name": "ldp_website"
+      },
+      {
+        "title": "Personal Email address ?",
+        "name": "ldp_email_address"
+      },
+      {
+        "title": "Linkedin profile url ?",
+        "name": "ldp_linkedin"
+      },
+      {
+        "title": "Twitter profile url ?",
+        "name": "ldp_twitter"
+      },
+      {
+        "title": "Github profile url ?",
+        "name": "ldp_github"
+      },
+      {
+        "title": "Facebook profile url ?",
+        "name": "ldp_facebook"
+      },
+      {
+        "title": "Vous connaissez : (URL)",
+        "name": "ldp_knows"
+      },
+      {
+        "title": "Vous connaissez : (URL)",
+        "name": "ldp_knows2"
+      },
+      {
+        "title": "Vous connaissez : (URL)",
+        "name": "ldp_knows3"
+      },
+      {
+        "title": "Vous connaissez : (URL)",
+        "name": "ldp_knows4"
+      }
+    ]
+  }
+};
